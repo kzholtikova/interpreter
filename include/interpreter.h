13a7956deb11f1a593fb9416312e781fa31aa766
Kira Zholtikova <kzholtikova@kse.org.ua>
2024-07-18 23:23:04 +0300

    refactored interpreter
    - extracted processExpression function

--- include/interpreter.h
+++ include/interpreter.h
@@ -1,8 +1,12 @@
 #pragma once
 #include <string>
 #include "token.h"
+#include "variablesStorage.h"
 
 class Interpreter {
 public:
+    static VariableStorage variables;
+
     static void processLine(const std::string& line);
+    static double processExpression(const std::string& line);
 };
