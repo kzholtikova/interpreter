84fc68985e0f0c24ff911d1fa2d3295fd6126e23
Kira Zholtikova <kzholtikova@kse.org.ua>
2024-07-18 20:29:43 +0300

    functions interpretation

--- src/interpreter.cpp
+++ src/interpreter.cpp
@@ -1,8 +1,17 @@
 #include <iostream>
 #include "../include/interpreter.h"
 #include "../include/calculator.h"
+#include "../include/functionHandler.h"
 
 void Interpreter::processLine(const std::string& line) {
-    double result = Calculator::calculateExpression(line);
+    double result;
+    size_t delimPos;
+    if (std::isdigit(line[0]))
+        result = Calculator::calculateExpression(line);
+    else if ((delimPos = line.find('(')) != std::string::npos && line.back() == ')')
+        result = FunctionHandler::applyFunction(line, delimPos);
+    else
+        throw std::invalid_argument("Invalid syntax");
+
     std::cout << result << "\n";
 }
